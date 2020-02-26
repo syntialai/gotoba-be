9474cf929d1fce6be423de76f4293f6af9ce0b33
unknown <hendrasimz92@gmail.com>
2020-02-26 15:48:55 +0700

    init project

---
+++ goToba/goToba/src/main/java/com/example/goToba/GoTobaApplication.java
@@ -0,0 +1,13 @@
+package com.example.goToba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTobaApplication.class, args);
+	}
+
+}
