3311006e108b1dd54f74b8e5d963862f01684421
unknown <hendrasimz92@gmail.com>
2020-04-15 11:39:52 +0700

    belajar impl

--- App/goToba/goToba/src/main/java/com/example/goToba/belajar/impl/BelajarImpl.java
+++ App/goToba/goToba/src/main/java/com/example/goToba/belajar/impl/BelajarImpl.java
@@ -6,22 +6,23 @@ import reactor.core.publisher.Mono;
 /**
  * Created by Sogumontar Hendra Simangunsong on 12/04/2020.
  */
-public class BelajarImpl  implements Belajar {
+public class BelajarImpl implements Belajar {
+
     @Override
     public Mono<Integer> pangkat(int a, int b) {
         Mono<Integer> mono2 = Mono.just(a)
-                .flatMap(val ->  Mono.just(a))
+                .flatMap(val -> Mono.just(a))
                 .doOnNext(base -> System.out.println("Base = " + base))
-                .flatMap(val ->  Mono.just(b))
+                .flatMap(val -> Mono.just(b))
                 .doOnNext(exponen -> System.out.println("eksponen = " + exponen))
-                .flatMap(res -> power(a,b))
+                .flatMap(res -> power(a, b))
                 .doOnNext(res -> System.out.println("Hasil " + a + " pangkat " + b + " adalah : " + res));
         return (Mono<Integer>) mono2.subscribe();
     }
 
     @Override
     public Mono<Integer> power(int base, int exponen) {
-        return Mono.just((int)Math.pow(base,exponen));
+        return Mono.just((int) Math.pow(base, exponen));
     }
 
 }
